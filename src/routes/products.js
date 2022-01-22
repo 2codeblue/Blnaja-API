b1e28e4af4f4ef28ff450e52392a9467264507dd
Juan Tombeng <juantombeng03@gmail.com>
2022-01-23 00:00:34 +0700

    fixing catetgory product routes

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,7 @@ const validator = require('../middleware/validator')
 
 route.get('/', productController.getProducts)
 route.get('/:id', productController.getProductDetail)
-route.get('/category', productController.getProductCategory)
+route.get('/category/', productController.getProductCategory)
 route.post('/', validator.productFormValidation, productController.addProduct)
 route.post('/category', validator.productCategoryForm, productController.addProductCategory)
 route.put('/:id', validator.productFormValidation, productController.updateProduct)
