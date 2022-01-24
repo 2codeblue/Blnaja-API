8da5b05862a70ac21aee0c078739737d3b8e7667
Juan Tombeng <juantombeng03@gmail.com>
2022-01-25 03:46:02 +0700

    add item to cart flow

--- src/controllers/bags.js
+++ src/controllers/bags.js
@@ -6,26 +6,55 @@ const bagsQuery = require('../models/bags')
 const addItem = async (req, res, next) => {
     try {
         const { product_id, customer_bags_id, size, color, qty } = req.body
-        const bag_item_id = uuidv4()
-        const bagData = {
-            id : customer_bags_id
+        const customer_bag_1 = customer_bags_id
+        const customer_bag_2 = await bagsQuery.findCustomerBagsId(customer_bag_1)
+        console.log(customer_bag_2);
+        if (customer_bag_2.length === 0) {
+            const bag_item_id = uuidv4()
+            const itemData = {
+                id : bag_item_id,
+                customer_bags_id : customer_bag_1,
+                product_id : product_id,
+                size : size,
+                color : color,
+                quantity : qty
+            }
+            const bagData = {
+                id : customer_bag_1
+            }
+            const addIdToBag = await bagsQuery.addItemsToBags(bagData)
+            const addItemToBag = await bagsQuery.addItem(itemData)
+            const result = {
+                addIdToBag,
+                addItemToBag
+            }
+            commonHelper.response(res, result, 200, 
+                `New Bag is created with id : ${customer_bag_1} and product ${product_id} is added to this bag`, null)
+        } else if (customer_bag_1 === customer_bag_2[0].id) {
+            const bag_item_id_target = await bagsQuery.findItemToUpdateQuantity(customer_bag_1, product_id)
+            console.log(bag_item_id_target);
+            if (bag_item_id_target.length === 0) {
+                const bag_item_id = uuidv4()
+                const itemData = {
+                    id : bag_item_id,
+                    customer_bags_id : customer_bag_1,
+                    product_id : product_id,
+                    size : size,
+                    color : color,
+                    quantity : qty
+                }
+                const addItemToBag = await bagsQuery.addItem(itemData)
+                commonHelper.response(res, addItemToBag, 200, `Product ${product_id} is added to customer bags : ${customer_bag_1}`, null)
+            } else if (bag_item_id_target[0].customer_bags_id === customer_bag_1 && bag_item_id_target[0].product_id === product_id) {
+                const bag_item_id = bag_item_id_target[0].id
+                const bag_item_qty = bag_item_id_target[0].quantity + qty
+                const itemData = {
+                    quantity : bag_item_qty
+                }
+                const updateItemInBag = await bagsQuery.updateQuantityItem(itemData, customer_bag_1, bag_item_id)
+                commonHelper.response(res, updateItemInBag, 200, `Product ${bag_item_id}'s quantity in bag ${customer_bag_1} is updated`, null)
+            }
         }
-        const itemData = {
-            id : bag_item_id,
-            customer_bags_id : customer_bags_id,
-            product_id : product_id,
-            size : size,
-            color : color,
-            quantity : qty
-        }
-        console.log(itemData)
-        const addIdToBag = await bagsQuery.addItemsToBags(bagData)
-        const addItemToBag = await bagsQuery.addItem(itemData)
-        const result = {
-            addIdToBag,
-            addItemToBag
-        }
-        commonHelper.response(res, result, 200, `Product ${product_id} is added to customer bags : ${customer_bags_id}`, null)
     } catch (error) {
         console.log(error)
         const err = new createError.InternalServerError()
--- src/models/bags.js
+++ src/models/bags.js
@@ -26,12 +26,52 @@ const addItemsToBags = (customer_bags_id) => {
     })
 }
 
+const findCustomerBagsId = (customer_bags_id) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT id FROM customer_bags WHERE id = ?`
+        connection.query(sql, customer_bags_id, (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const findItemToUpdateQuantity = (customer_bags_id, product_id) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT id, customer_bags_id, product_id, quantity FROM bag_item WHERE customer_bags_id = ? AND product_id = ?`
+        connection.query(sql, [customer_bags_id, product_id], (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const updateQuantityItem = (data, customer_bags_id, bag_item_id) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `UPDATE bag_item SET ? WHERE customer_bags_id = ? AND id = ?`
+        connection.query(sql, [data, customer_bags_id, bag_item_id], (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
 const getItems = (customer_bags_id) => {
     return new Promise ((resolve, reject) => {
         const sql = `SELECT bag_item.id, bag_item.product_id, bag_item.size, bag_item.color, 
-        bag_item.quantity, products.name, sellers.store_name FROM products INNER JOIN 
-        bag_item ON products.id = bag_item.product_id INNER JOIN sellers ON products.seller_id = 
-        sellers.id WHERE bag_item.id = ?`
+        bag_item.quantity, products.name, products.price, products.image1, sellers.id as seller_id, 
+        sellers.store_name FROM products INNER JOIN bag_item ON products.id = bag_item.product_id 
+        INNER JOIN sellers ON products.seller_id = sellers.id 
+        WHERE bag_item.customer_bags_id = ?`
         connection.query(sql, customer_bags_id, (error, result) => {
             if (!error) {
                 resolve(result)
@@ -47,5 +87,8 @@ const getItems = (customer_bags_id) => {
 module.exports = {
     addItem,
     addItemsToBags,
+    findCustomerBagsId,
+    findItemToUpdateQuantity,
+    updateQuantityItem,
     getItems
 }
