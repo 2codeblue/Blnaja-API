b93fbdaa78c03c5b1bdd8d548d70b2eda301c1bf
Juan Tombeng <juantombeng03@gmail.com>
2022-04-07 21:20:30 +0700

    fixing any value error

--- src/models/orders.js
+++ src/models/orders.js
@@ -55,7 +55,7 @@ const checkOrderByCustomerBag = (customer_bags_id) => {
 const getOrdersByCustomerId = (customer_id) => {
     return new Promise ((resolve, reject) => {
         const sql = `SELECT orders.id, orders.status, ANY_VALUE(customer_bags.total_price), ANY_VALUE(customer_bags.total_quantity), 
-        bag_item.product_id FROM customer_bags INNER JOIN orders ON customer_bags.order_id = orders.id INNER JOIN 
+        ANY_VALUE(bag_item.product_id) FROM customer_bags INNER JOIN orders ON customer_bags.order_id = orders.id INNER JOIN 
         bag_item ON customer_bags.id = bag_item.customer_bags_id WHERE orders.customer_id = ? GROUP BY customer_bags.order_id`
         connection.query(sql, customer_id, (error, result) => {
             if (!error) {
