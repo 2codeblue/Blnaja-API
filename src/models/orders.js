8814c3815658e768013617096e8d3e7e9b935754
Juan Tombeng <juantombeng03@gmail.com>
2022-04-07 22:02:19 +0700

    typo asc desc

--- src/models/orders.js
+++ src/models/orders.js
@@ -56,7 +56,7 @@ const getOrdersByCustomerId = (customer_id) => {
     return new Promise ((resolve, reject) => {
         const sql = `SELECT orders.id, orders.status, ANY_VALUE(customer_bags.total_price) AS total_price, ANY_VALUE(customer_bags.total_quantity) AS total_quantity, 
         ANY_VALUE(bag_item.product_id) AS product_id FROM customer_bags INNER JOIN orders ON customer_bags.order_id = orders.id INNER JOIN 
-        bag_item ON customer_bags.id = bag_item.customer_bags_id WHERE orders.customer_id = ? GROUP BY customer_bags.order_id ORDER BY orders.created_at DESC`
+        bag_item ON customer_bags.id = bag_item.customer_bags_id WHERE orders.customer_id = ? GROUP BY customer_bags.order_id ORDER BY orders.created_at ASC`
         connection.query(sql, customer_id, (error, result) => {
             if (!error) {
                 resolve(result)
